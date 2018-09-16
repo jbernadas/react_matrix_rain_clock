94ec58a2843b1bfc6d5f1acadf944bf9e82c7819
jb3rns <jb3rns@gmail.com>
2018-09-16 14:14:05 -0500

    Removed serviceworkers on master

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Clock from './Clock';
 import Matrix from './Matrix';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Matrix />, document.getElementById('canvas-wrapper'));
 ReactDOM.render(<Clock />, document.getElementById('root'));
-registerServiceWorker();
